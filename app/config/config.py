5b593c44636a1deaf33114b755bed76a53074f46
) <kuropatenko18@gmail.com>
2025-07-25 20:49:39 +0200

    added conf file and project tree

---
+++ app/config/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    app_name: str = "Airport API"
+    debug: bool = False
+
+    db_dsn: str
+
+    redis_dsn: str
+
+    rabbit_dsn: str
+
+    jwt_secret: str
+    jwt_algorithm:str = "HS256"
+    jwt_access_expires: int = 3600
+
+    model_config = SettingsConfigDict(
+        env_file= ".env",
+        env_file_encoding= "utf-8"
+    )
+
+settings = Settings()
